e1c2814bb913d4cf8b6e329cf9bcb3c56932031c
luotao <gtkrockets@163.com>
2021-04-01 10:26:11 +0800

    修改无屏音效配网协议

---
+++ app/src/main/java/com/smartwasp/assistant/app/bean/ApBean.kt
@@ -0,0 +1,9 @@
+package com.smartwasp.assistant.app.bean
+
+/**
+ * Created by luotao on 2021/3/30 17:50
+ * E-Mail Address：gtkrockets@163.com
+ */
+data class ApBean(var client_id:String,
+                  var pid:String,
+                  var did:String)
